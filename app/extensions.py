434afe68f4107620d5f0ce27f439ed127b5b3b3b
bugi4321 <lbugarija23@student.unizd.hr>
2025-11-20 14:14:18 +0100

    Maknut global limiter

--- app/extensions.py
+++ app/extensions.py
@@ -14,7 +14,7 @@ login_manager = LoginManager()
 
 limiter = Limiter(
     key_func=get_remote_address,
-    default_limits=["200 per day", "50 per hour"],  
+    default_limits=[],  
     storage_uri="memory://",  
     strategy="fixed-window"  
 )
